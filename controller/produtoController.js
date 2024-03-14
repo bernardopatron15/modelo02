91c29cd48bb8a9b29e707ff0618ca7138df9196f
bernardopatron15 <bernardopatron.bg025@academico.ifsul.edu.br>
2024-03-14 15:33:26 -0300

    c

--- controller/produtoController.js
+++ controller/produtoController.js
@@ -104,45 +104,7 @@ function edt(req, res) {
                     res.send(err);
                 } else {
                     res.redirect('/produto/lst')
-                }const express = require('express');
-                const path = require('path');
-                const app = express();const express = require('express');
-                const path = require('path');
-                const app = express();
-                const port = process.env.PORT || 3000
-                
-                app.use(express.urlencoded({extended:true}))
-                app.set('view engine', 'ejs');
-                
-                app.use(express.static(path.join(__dirname, 'public')));
-                
-                const usuarioroutes = require('./routes/UsuarioRoutes')
-                const produtoroutes = require('./routes/ProdutoRoutes')
-                const postRoutes = require('./routes/PostRoutes')
-                app.use(usuarioroutes)
-                app.use(produtoroutes)
-                app.use(postRoutes)
-                
-                app.listen(port, function(){
-                    console.log('Servidor funcionando!')
-                })
-                const port = process.env.PORT || 3000
-                
-                app.use(express.urlencoded({extended:true}))
-                app.set('view engine', 'ejs');
-                
-                app.use(express.static(path.join(__dirname, 'public')));
-                
-                const usuarioroutes = require('./routes/UsuarioRoutes')
-                const produtoroutes = require('./routes/ProdutoRoutes')
-                const postRoutes = require('./routes/PostRoutes')
-                app.use(usuarioroutes)
-                app.use(produtoroutes)
-                app.use(postRoutes)
-                
-                app.listen(port, function(){
-                    console.log('Servidor funcionando!')
-                })
+                }
             })
         }
     })
