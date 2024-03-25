3bda5845fa87b34ef881460ffad3726383e33b1c
bernardopatron15 <bernardopatron.bg025@academico.ifsul.edu.br>
2024-03-25 14:18:45 -0300

    k

--- controller/produtoController.js
+++ controller/produtoController.js
@@ -1,93 +1,107 @@
-const Produto = require('../model/Produto');
+const Produto = require("../model/Produto");
 
 function abreadd(req, res) {
-    res.render('produto/add');
+  res.render("produto/add");
 }
 
 function add(req, res) {
-    let produto = new Produto({
-        titulo: req.body.titulo,
-        descricao: req.body.descricao,
-        categoria: req.body.categoria,
-        preco: parseInt(req.body.preco),
-        foto: req.file.filename,
-    });
+  let produto = new Produto({
+    titulo: req.body.titulo,
+    descricao: req.body.descricao,
+    categoria: req.body.categoria,
+    preco: parseInt(req.body.preco),
+    foto: req.file.filename,
+  });
 
-    produto.save().then(function (produto) {
-        res.redirect("/produto/add");
-    }).catch(function (err) {
-        res.send(err);
+  produto
+    .save()
+    .then(function (produto) {
+      res.redirect("/produto/add");
+    })
+    .catch(function (err) {
+      res.send(err);
     });
 }
 
 function listar(req, res) {
-    Produto.find({}).then(function (produtos) {
-        res.render('produto/lst', {
-            Produtos: produtos
-        });
-    }).catch(function (err) {
-        res.send(err);
+  Produto.find({})
+    .then(function (produtos) {
+      res.render("produto/lst", {
+        Produtos: produtos,
+      });
+    })
+    .catch(function (err) {
+      res.send(err);
     });
 }
 
 function filtrar(req, res) {
-    Produto.find({
-        titulo: new RegExp(req.body.pesquisar.split(' ').join('.*'), 'ig')
-    }).then(function (produtos) {
-        res.render('produto/lst', {
-            Produtos: produtos
-        });
-    }).catch(function (err) {
-        res.send(err);
+  Produto.find({
+    titulo: new RegExp(req.body.pesquisar.split(" ").join(".*"), "ig"),
+  })
+    .then(function (produtos) {
+      res.render("produto/lst", {
+        Produtos: produtos,
+      });
+    })
+    .catch(function (err) {
+      res.send(err);
     });
 }
 
 function del(req, res) {
-    Produto.findByIdAndDelete(req.params.id).then(function () {
-        res.redirect('/produto/lst');
-    }).catch(function (err) {
-        res.send(err);
+  Produto.findByIdAndDelete(req.params.id)
+    .then(function () {
+      res.redirect("/produto/lst");
+    })
+    .catch(function (err) {
+      res.send(err);
     });
 }
 
 function abreedt(req, res) {
-    Produto.findById(req.params.id).then(function (produto) {
-        res.render('produto/edt', {
-            Produto: produto
-        });
-    }).catch(function (err) {
-        res.send(err);
+  Produto.findById(req.params.id)
+    .then(function (produto) {
+      res.render("produto/edt", {
+        Produto: produto,
+      });
+    })
+    .catch(function (err) {
+      res.send(err);
     });
 }
 
 function edt(req, res) {
-    Produto.findById(req.params.id).then(function (produto) {
-        let tempo = req.body.tempo;
-        let [hora, minuto] = tempo.split(':');
-        let data = new Date();
-        data.setHours(hora);
-        data.setMinutes(minuto);
-        
-        produto.titulo = req.body.titulo;
-        produto.descricao = req.body.descricao;
-        produto.categoria = req.body.categoria;
-        produto.preco = req.body.preco;
-        produto.foto = req.body.foto;
-        
-        return produto.save();
-    }).then(function () {
-        res.redirect('/produto/lst');
-    }).catch(function (err) {
-        res.send(err);
+  Produto.findById(req.params.id)
+    .then(function (produto) {
+      let tempo = req.body.tempo;
+      let [hora, minuto] = tempo.split(":");
+      let data = new Date();
+      data.setHours(hora);
+      data.setMinutes(minuto);
+
+      produto.titulo = req.body.titulo;
+      produto.descricao = req.body.descricao;
+      produto.categoria = req.body.categoria;
+      produto.preco = req.body.preco;
+      produto.foto = req.body.foto;
+
+      return produto.save();
+    })
+    .then(function () {
+      res.redirect("/produto/lst");
+    })
+    .catch(function (err) {
+      res.send(err);
     });
 }
 
 module.exports = {
-    abreadd,
-    add,
-    listar,
-    filtrar,
-    abreedt,
-    edt,
-    del
+  abreadd,
+  add,
+  listar,
+  filtrar,
+  abreedt,
+  edt,
+  del,
 };
--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.use(express.urlencoded({extended:true}))
-app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-const usuarioroutes = require('./routes/UsuarioRoutes')
-const produtoroutes = require('./routes/ProdutoRoutes')
-const postRoutes = require('./routes/PostRoutes')
-app.use(usuarioroutes)
-app.use(produtoroutes)
-app.use(postRoutes)
+const usuarioroutes = require("./routes/UsuarioRoutes");
+const produtoroutes = require("./routes/ProdutoRoutes");
+const postRoutes = require("./routes/PostRoutes");
+app.use(usuarioroutes);
+app.use(produtoroutes);
+app.use(postRoutes);
 
-app.listen(port, function(){
-    console.log('Servidor funcionando!')
-})
+app.listen(port, function () {
+  console.log("Servidor funcionando!");
+});
